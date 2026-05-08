949c4834966ca49c36acb7daf329d27abb90bcb9
Abdul Rafay <vengeance_11@outlook.com>
2026-05-07 22:04:03 -0400

    environment + modules defined

---
+++ terraform-beginner-project/environments/modules/s3_bucket/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Globally unique S3 bucket name"
+  type        = string
+}
