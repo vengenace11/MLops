6a0c50a02be048dc0e698f0bc3ef3bb39dd3475b
Abdul Rafay <vengeance_11@outlook.com>
2026-05-09 22:48:53 -0400

    deploying s3 only, commemted other resources

--- terraform-beginner-project/environments/dev/main.tf
+++ terraform-beginner-project/environments/dev/main.tf
@@ -19,32 +19,6 @@ provider "aws" {
   profile = var.aws_profile
 }
 
-# data "aws_vpc" "default" {
-  default = true
-# }
-
-# data "aws_subnets" "default" {
-#   filter {
-#     name   = "vpc-id"
-#     values = [data.aws_vpc.default.id]
-#   }
-# }
-
-# # data "aws_ami" "ubuntu" {
-# #   most_recent = true
-# #   owners      = ["099720109477"]
-
-# #   filter {
-# #     name   = "name"
-# #     values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-# #   }
-
-# #   filter {
-# #     name   = "virtualization-type"
-# #     values = ["hvm"]
-# #   }
-# # }
-
 module "s3_bucket" {
   source = "../../modules/s3_bucket"
 
@@ -52,26 +26,3 @@ module "s3_bucket" {
   environment        = var.environment
   project_name       = var.project_name
 }
-
-# # module "security_group" {
-#   source = "../../modules/security_group"
-
-#   security_group_name = var.security_group_name
-#   vpc_id              = data.aws_vpc.default.id
-#   allowed_ssh_cidr    = var.allowed_ssh_cidr
-#   environment         = var.environment
-#   project_name        = var.project_name
-# }
-
-# # module "ec2_instance" {
-#   source = "../../modules/ec2_instance"
-
-# #   ami_id             = data.aws_ami.ubuntu.id
-# #   instance_type      = var.instance_type
-# #   subnet_id          = data.aws_subnets.default.ids[0]
-# #   security_group_ids = [module.security_group.security_group_id]
-# #   key_name           = var.key_name
-# #   instance_name      = var.instance_name
-# #   environment        = var.environment
-# #   project_name       = var.project_name
-# }
