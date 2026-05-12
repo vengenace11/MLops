84f3283b2a71cb7bbd7e300bd5f2d347e553ed1a
Abdul Rafay <vengeance_11@outlook.com>
2026-05-12 17:40:59 -0400

    tf files for eks deployment

--- terraform-beginner-project/modules/s3_bucket/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_name" {
-  description = "Name of the S3 bucket"
-  value       = aws_s3_bucket.this.bucket
-}
-
-output "bucket_id" {
-  description = "ID of the S3 bucket"
-  value       = aws_s3_bucket.this.id
-}
