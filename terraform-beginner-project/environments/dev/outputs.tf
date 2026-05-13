f15170a32c0b62cc33a4cc69732911f773ba10a7
Abdul Rafay <vengeance_11@outlook.com>
2026-05-12 21:09:52 -0400

    fixing terraform.tfvars

--- terraform-beginner-project/environments/dev/outputs.tf
+++
@@ -1,9 +0,0 @@
-# outputs.tf
-
-output "cluster_name" {
-  value = module.eks.cluster_name
-}
-
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
--- terraform-beginner-project/environments/dev/variables.tf
+++
@@ -1,6 +0,0 @@
-# variables.tf
-
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
